cd817f8364bb4e3add0542eaf5d1ae51a5b9506f
Justin Bieber <johnwangfirst@mgmailcom>
2017-01-28 13:11:41 -0600

    gave kunkka skill build
    only for skills, no talents yet
    hopefully I can extend this to everyone

--- ability_item_usage_kunkka.lua
+++ ability_item_usage_kunkka.lua
@@ -9,3 +9,29 @@ function AbilityUsageThink()
 
 end
 
+
+local skillbuild = {3,2,3,2,3,
+					4,3,2,2,1,
+					1,1,1,4,4}
+
+function AbilityLevelUpThink()
+
+						
+	local abilities =  {"kunkka_torrent",
+"kunkka_tidebringer",
+"kunkka_x_marks_the_spot",
+"kunkka_ghostship"}
+						
+					
+	
+	local ability = GetBot():GetAbilityByName(abilities[skillbuild[1]]);
+	          
+	if( ability:CanAbilityBeUpgraded() and ability:GetLevel() < ability:GetMaxLevel())  then
+	
+	    
+		GetBot():Action_LevelAbility(ability:GetName())
+		table.remove(skillbuild, 1)
+	end
+	
+end
+
